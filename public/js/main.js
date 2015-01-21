83a8d2ee00bfed14a56be9e14c00b210b350db77
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-01-22 01:08:28 +1100

    changes on default and the css

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,26 @@
-
 window.onload = function(){
-    // x functionality when window loads
-    	fartscroll();
+	// $(window).resize(function() {
+	// 		var width = $(document).width();
+	// 		if ( width < 1000 ) {
 
+	// 			// var $document = $(document),
+	// 			//     $element = $(".menu-button"),
+	// 			//     className = ".menu-button-hidden";
+
+	// 			// $document.scroll(function() {
+	// 			//   $element.toggleClass(className, $document.scrollTop() >= 50);
+	// 			// });		
+
+	// 			// $(".menu-button").fadIn("fast").delay(1000).fadeOut("slow"); // Fades out the menu-button after 1 second
+	// 			$("button.menu-button").toggleClass("button.menu-button-hidden");
+	// 		}
+	// });
+	$(window).scroll(function() {
+	    if ($(this).scrollTop() > 100) {
+	        $('.menu-button').fadeIn();
+	    } else {
+	        $('.menu-button').fadeOut();
+	    }
+	});​
 }
+
