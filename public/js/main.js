1de2180f07a1d91b09edd0748dc96b125d91c2e1
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-01-22 16:20:34 +1100

    adding stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,39 @@
-window.onload = function(){
-	// $(window).resize(function() {
-	// 		var width = $(document).width();
-	// 		if ( width < 1000 ) {
+// $(document).ready( function(){
+// 	$(window).resize(function() {
+// 			var width = $(document).width();
+// 			if ( width < 1000 ) {
 
-	// 			// var $document = $(document),
-	// 			//     $element = $(".menu-button"),
-	// 			//     className = ".menu-button-hidden";
+// 				var $document = $(document),
+// 				    $element = $(".menu-button"),
+// 				    className = ".menu-button-hidden";
 
-	// 			// $document.scroll(function() {
-	// 			//   $element.toggleClass(className, $document.scrollTop() >= 50);
-	// 			// });		
+// 				$document.scroll(function() {
+// 				  $element.toggleClass(className, $document.scrollTop() >= 50);
+// 				});		
 
-	// 			// $(".menu-button").fadIn("fast").delay(1000).fadeOut("slow"); // Fades out the menu-button after 1 second
-	// 			$("button.menu-button").toggleClass("button.menu-button-hidden");
-	// 		}
-	// });
-	$(window).scroll(function() {
-	    if ($(this).scrollTop() > 100) {
-	        $('.menu-button').fadeIn();
-	    } else {
-	        $('.menu-button').fadeOut();
-	    }
-	});​
-}
+// 				$(".menu-button").delay(1000).fadeOut("slow"); // Fades out the menu-button after 1 second
+// 				$("button.menu-button").toggleClass("button.menu-button-hidden");
+// 			}
+// 	});
+
+	// $(window).scroll(function() {
+	//     if ($(this).scrollTop() > 100) {
+	//         $('.menu-button').fadeIn();
+	//     } else {
+	//         $('.menu-button').fadeOut();
+	//     }
+	// })
+// });
+
+
+// $(document).ready(function() {
+
+// 	var button = $(".menu-button");
+// 	$(window).scroll(function() {
+// 		if ( $(window).scrollTop() > 50 ) {
+
+// 		}
+// 	});
+
+// });
 
